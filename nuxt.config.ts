c29bc2305de422d0e34172d57b0fd4d81115e877
Mehdi Shah abbasian <mehdi.shahabbasian@gmail.com>
2022-09-01 15:01:57 +0430

    config: install required plugin and assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,38 @@
 import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  meta: {
+    htmlAttrs: {
+      dir: 'rtl'
+    },
+    title: 'JRI - TODO List',
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', sizes: '32x32', type: 'image/png', href: '/favicon-32x32.png' },
+      { rel: 'icon', sizes: '16x16', type: 'image/png', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#192f64' }
+    ],
+    meta: [
+      { name: 'msapplication-TileColor', content: '#192f64' },
+      { name: 'theme-color', content: '#192f64' }
+    ]
+  },
+  css: [
+    '~/assets/css/fontiran.css'
+  ],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@formkit/nuxt',
+    '@vueuse/nuxt',
+    '@pinia/nuxt'
+  ],
+  formkit: {
+    defaultConfig: true,
+    configFile: '~/formkit.config.ts'
+  },
+  tailwindcss: {
+    viewer: false
+  }
 })
