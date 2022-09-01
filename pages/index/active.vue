f0a6bb28ab80927cfd442380cfad3a12614e0237
Mehdi Shah abbasian <mehdi.shahabbasian@gmail.com>
2022-09-01 21:34:32 +0430

    feat: add Head and Meta

--- pages/index/active.vue
+++ pages/index/active.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <Head>
+      <Title>JRI - Active Todo</Title>
+      <Meta name="description" content="Active Todo" />
+    </Head>
+
     <List :todos="useTodo().activeTodo" />
   </div>
 </template>
--- pages/index/completed.vue
+++ pages/index/completed.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <Head>
+      <Title>JRI - Completed Todo</Title>
+      <Meta name="description" content="Completed Todo" />
+    </Head>
+
     <List :todos="useTodo().completedTodo" />
   </div>
 </template>
